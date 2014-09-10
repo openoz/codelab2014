e4bb7f971094d1d73aa47e46c5ddd1c62f6672c7
Kiran Devarakonda <kdevarakonda@aamc.org>
2014-09-10 11:29:09 -0400

    fixing ie issues

--- Gruntfile.js
+++ Gruntfile.js
@@ -69,9 +69,8 @@ module.exports = function(grunt) {
     copy: {
       html: {
         options: {
-          process: function(content, srcpath) {
-            console.log(srcpath);
-            return content.replace(/href="\//g, 'href="/aamc-styles/');
+          process: function(content, srcpath) {            
+            return content.replace(/href="\//g, 'href="/aamc-styles/').replace("http://localhost:8888/", "https://apps.development.aamc.org/aamc-styles");
           }
         },
         expand: true,
